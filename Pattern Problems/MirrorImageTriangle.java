fd23c32f4a0b245e0676f1f69c479f7b8f265e55
Sahil Dhene <162948738+SahilDhene@users.noreply.github.com>
2024-07-23 23:13:48 +0530

    Mirror Image Triangle Pattern

---
+++ Pattern Problems/MirrorImageTriangle.java
@@ -0,0 +1,23 @@
+class MirrorImageTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
