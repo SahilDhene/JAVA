c1b51a7b9dadd07ff55bd3c24db66ac5cd0437c6
Sahil Dhene <162948738+SahilDhene@users.noreply.github.com>
2024-07-28 12:12:44 +0530

    Implement an algorithm to find the "Kth" smallest element in an array.

---
+++ Array Problems/FindKthSmallest.java
@@ -0,0 +1,21 @@
+public class FindKthSmallest {
+    public static void main(String[] args) {
+        int[] arr = {3, 6, 2, 1, 4};
+        int k = 2;
+        int kthSmallest = findKthSmallest(arr, k);
+        System.out.print("Kth smallest element: " + kthSmallest);
+    }
+
+    public static int findKthSmallest(int[] array, int k) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array[k - 1];
+    }
+}
